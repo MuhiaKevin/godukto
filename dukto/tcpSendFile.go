2f8e4d86c63ca4babe01aeba2f096a684494d690
MuhiaKevin <archerkreiger@gmail.com>
2023-02-15 18:28:36 +0300

    add support for commandline argurements

--- dukto/tcpSendFile.go
+++ dukto/tcpSendFile.go
@@ -7,20 +7,24 @@ import (
 	"os"
 )
 
+const port = 4644
+
 func CheckErr(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
-func SendFile(fileName string) {
+func SendFile(fileName string, host string) {
+	client := fmt.Sprintf("%s:%d", host, port)
+
 	file, err := os.Open(fileName)
 
 	CheckErr(err)
 
 	defer file.Close()
 
-	conn, err := net.Dial("tcp", "192.168.1.195:4644")
+	conn, err := net.Dial("tcp", client)
 	defer conn.Close()
 
 	CheckErr(err)
--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func main() {
+	host := flag.String("h", "192.168.1.195", "set host")
 	file := flag.String("f", "", "path to file")
+
 	flag.Parse()
 
 	if *file == "" {
 		panic("Enter a file")
 	}
-
-	dukto.SendFile(*file)
+	dukto.SendFile(*file, *host)
 }
